60b3c4db70a59d3ac11fdb2677dca6e0c8c7e873
Yogita24496 <yogita.patil@hurix.com>
2025-05-27 19:25:51 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders Weather Forecast title', () => {
+  render(<App />);
+  const titleElement = screen.getByText(/Weather Forecast/i);
+  expect(titleElement).toBeInTheDocument();
+});
